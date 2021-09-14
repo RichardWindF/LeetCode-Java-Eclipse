ffda65cfcb07cb1d289ee770f6ceca3b298a3d1c
RichardWindF <romewind1@gmail.com>
2021-09-14 13:33:54 -0400

    first test git eclipse

---
+++ src/com/leetCodeDemo/easy/Number2Add.java
@@ -0,0 +1,16 @@
+package com.leetCodeDemo.easy;
+
+public class Number2Add
+{
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		//
+		
+		
+		//test for git
+
+	}
+
+}
